86b37b4fec98fd7a70ffd8e3bda89773e7ffc045
aslanliIbrahim <ibrahimra@code.edu.az>
2024-08-30 12:45:02 +0400

    BestSelller side is ready for use

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -5,15 +5,20 @@ import { Link } from "react-router-dom";
 const ProductItem = ({ id, name, image, price }) => {
   const { currency } = useContext(ShopContext);
 
-  console.log(id, "this is id");
-  
   return (
     <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
-        <div className="overflow-hidden">
-            <img src={image[0]} alt={name} className="hover:scale-110 transition ease-in-out" />
-        </div>
-        <p className="pt-3 pb-1 text-sm">{name}</p>
-        <p className="text-sm font-medium">{currency}{price}</p>
+      <div className="overflow-hidden">
+        <img
+          src={image[0]}
+          alt={name}
+          className="hover:scale-110 transition ease-in-out"
+        />
+      </div>
+      <p className="pt-3 pb-1 text-sm">{name}</p>
+      <p className="text-sm font-medium">
+        {currency}
+        {price}
+      </p>
     </Link>
   );
 };

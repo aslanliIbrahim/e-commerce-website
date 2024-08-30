9c340931f106d4573fd3cb07c6aadd81dece2eb0
aslanliIbrahim <ibrahimra@code.edu.az>
2024-08-31 00:33:06 +0400

    collection right side ui is ready for use

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,11 +1,17 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/frontend_assets/assets";
+import Title from "../components/Title";
+import ProductItem from "../components/ProductItem";
 
 const Collection = () => {
   const { products } = useContext(ShopContext);
   const [showList, setShowList] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
 
+  useEffect(() => {
+    setFilterProducts(products);
+  }, []);
 
   return (
     <section className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
@@ -69,6 +75,29 @@ const Collection = () => {
               Winterwear
             </label>
           </div>
+        </div>
+      </div>
+      {/*Right Side */}
+      <div className="flex-1">
+        <div className="flex justify-between text-base sm:text-2xl mb-4">
+          <Title text1={"all"} text2={"collections"} />
+          <select className="border-2 border-gray-300 text-sm px-2">
+            <option value="relevent">Sort by: Relevent</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to low</option>
+          </select>
+        </div>
+        {/*Product's side */}
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
+          {filterProducts.map((item, index) => (
+            <ProductItem
+              key={index}
+              name={item.name}
+              id={item._id}
+              price={item.price}
+              image={item.image}
+            />
+          ))}
         </div>
       </div>
     </section>

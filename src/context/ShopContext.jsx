33a2dc0fda2970bbbad4b4757c53a125a45d7460
aslanliIbrahim <ibrahimra@code.edu.az>
2024-08-31 11:38:15 +0400

    SearchBar was created

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,12 +1,20 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
   const currency = "$";
-  const delivery_fee =10
+  const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(true);
   const value = {
-    products, currency, delivery_fee
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch
   };
 
   return (

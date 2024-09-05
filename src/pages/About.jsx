2db341ee0e46551d0506a5befeddb0842a8d3011
aslanliIbrahim <ibrahimra@code.edu.az>
2024-09-05 12:22:40 +0400

    project was ready for use

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 const about = () => {
   return (
-    <div>about</div>
-  )
-}
+    <section>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"about"} text2={"us"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt="about_img"
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            beatae totam dolorem praesentium ipsum neque nisi enim. Molestias,
+            quaerat ab praesentium itaque in magnam, ipsam delectus omnis nemo
+            tempore sint. Corporis fugit officiis vero. Amet!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            beatae totam dolorem praesentium ipsum neque nisi enim. Molestias,
+            quaerat ab praesentium itaque in magnam, ipsam delectus omnis nemo
+            tempore sint. Corporis fugit officiis vero. Amet!
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            beatae totam dolorem praesentium ipsum neque nisi enim. Molestias,
+            quaerat ab praesentium itaque in magnam, ipsam delectus omnis nemo
+            tempore sint.
+          </p>
+        </div>
+      </div>
+      <div className="text-xl py-4">
+        <Title text1={"Why"} text2={"choose Us"} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            beatae totam dolorem praesentium ipsum neque nisi enim. Molestias,
+            quaerat ab praesentium itaque in magnam, ipsam delectus omnis nemo
+            tempore sint.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            beatae totam dolorem praesentium ipsum neque nisi enim. Molestias,
+            quaerat ab praesentium itaque in magnam, ipsam delectus omnis nemo
+            tempore sint.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assuarnce</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            beatae totam dolorem praesentium ipsum neque nisi enim. Molestias,
+            quaerat ab praesentium itaque in magnam, ipsam delectus omnis nemo
+            tempore sint.
+          </p>
+        </div>
+      </div>
 
-export default about
+      <NewsLetterBox/>
+    </section>
+  );
+};
+
+export default about;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
 
 const contact = () => {
   return (
-    <div>contact</div>
-  )
-}
+    <section>
+      <div>
+        <div className="text-center text-2xl pt-10 border-t">
+          <Title text1={"Contact"} text2={"Us"} />
+        </div>
+        <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+          <img
+            src={assets.contact_img}
+            alt="contact_img"
+            className="w-full md:max-w-[480px]"
+          />
+          <div className=" flex flex-col justify-center items-start gap-6">
+            <p className="font-semibold text-xl text-gray-600">Our Store</p>
+            <p className="text-gray-600">
+              silicon valley <br /> Suite 350, America{" "}
+            </p>
+            <p className="text-gray-500">
+              +994 (51)-334-22-88 <br /> Email:aslanov.ibrahim061@gmail.com
+            </p>
+            <p className="text-gray-500"></p>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default contact
+export default contact;

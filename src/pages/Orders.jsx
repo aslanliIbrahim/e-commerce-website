9583a9ceef89fefeaac8a1fd2057f2fa2f7a9f0b
aslanliIbrahim <ibrahimra@code.edu.az>
2024-09-05 11:29:03 +0400

    order page was ready for use

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
 
 const Orders = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { products, currency } = useContext(ShopContext);
 
-export default Orders
+  return (
+    <section className="border-t pt-16">
+      <div className="text-2xl">
+        <Title text1={"My"} text2={"orders"} />
+      </div>
+      <div className="">
+        {products.slice(1, 4).map((items, index) => (
+          <div
+            key={index}
+            className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
+          >
+            <div className="flex items-start gap-6 text-sm">
+              <img className="w-16 sm:w-20" src={items.image[0]} alt="images" />
+              <div>
+                <p className="sm:text-base font-medium">{items.name}</p>
+                <div className="flex items-center gap-3 mt-2 text-base text-gray-700 ">
+                  <p className="text-lg">
+                    {currency}
+                    {items.price}
+                  </p>
+                  <p>Quantity: 1</p>
+                  <p>Size: M</p>
+                </div>
+                <p className="">
+                  Date <span className="text-gray-700">01, Jul, 2024</span>
+                </p>
+              </div>
+            </div>
+            <div className="md:w-1/2 flex justify-between">
+              <div className="flex items-center gap-2">
+                <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
+                <p className="text-sm md:test-base">Ready to ship</p>
+              </div>
+              <button className="border px-4 py-2 text-sm font-medium rounded-sm">Track order</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Orders;
